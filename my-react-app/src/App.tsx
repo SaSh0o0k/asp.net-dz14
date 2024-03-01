0356ea1b4c617153f615a857b4592551bae9eeca
Oleksandr Putsyk <sashaputsyk100903@gmail.com>
2024-03-01 19:56:24 +0200

    Initial Commit

---
+++ my-react-app/src/App.tsx
@@ -0,0 +1,23 @@
+
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+import CategoryListPage from "./components/categories/list/CategoryListPage.tsx";
+import CategoryCreatePage from "./components/categories/create/CategoryCreatePage.tsx";
+import CategoryEditPage from "./components/categories/edit/CategoryEditPage.tsx";
+
+const App: React.FC = () => {
+
+    return (
+        <>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<CategoryListPage />} />
+                    <Route path="create" element={<CategoryCreatePage />} />
+                    <Route path='edit/:id' element={<CategoryEditPage />} />
+                </Route>
+            </Routes>
+        </>
+    )
+}
+
+export default App
